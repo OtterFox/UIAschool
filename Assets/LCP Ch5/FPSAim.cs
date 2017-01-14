4fa79d23ca5168ed8f860242d651ef26c081ff7e
Otter Fox <otterfoxgames@gmail.com>
2017-01-13 18:22:36 -0700

    This
    and that

---
+++ Assets/LCP Ch5/FPSAim.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSAim : MonoBehaviour {
+
+    float mouseX;
+    float mouseY;
+
+    bool isInverted;
+
+	void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+        MousPos();
+        Inverted();
+    }
+
+   public void MousPos()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        if(isInverted)
+        {
+            mouseY += Input.GetAxis("Mouse Y");
+        }
+        else
+        {
+            mouseY -= Input.GetAxis("Mouse Y"); //view up/down w/out -= up/down would be reversed
+        }
+       
+        print(mouseX);
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+
+    void Inverted() //inverts up/down
+    {
+        if(!isInverted && Input.GetKey(KeyCode.P))
+        {
+            isInverted = true;
+            print("mouseY Inverted");
+        }
+        else if (isInverted && Input.GetKey(KeyCode.P))
+        {
+            isInverted = false;
+            print("mouseY is NOT Inverted");
+        }
+        else
+        {
+            print("Something went wrong, does up/down work?");
+        }
+    }
+
+
+}
---
+++ Assets/LCP Ch5/FPSMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSMove : MonoBehaviour {
+
+    public float speed= 0.01f;
+
+	void Start ()
+    {
+	
+	}
+	
+
+	void FixedUpdate ()
+    {
+        FlightCntrl();
+
+    }
+
+    void FlightCntrl()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
+        }
+    }
+}
